0232f1cbccaed929565ec9c6c6e89857afe03a06
Aditya Agarwal <adityaa803@gmail.com>
2018-05-20 03:42:24 +0530

    fixes #1 add sentiment script

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const backgroundFinder = require('./lib/backgroundFinder')
+
 module.exports = (robot) => {
   robot.log('bot started')
-  robot.on(['installation.created', 'installation_repositories.added'], context => {
+  robot.on('issue_comment.created', context => {
     context.log('event received')
-    context.log('data received', context.payload)
+    context.log('repo details', context.repo())
+    backgroundFinder(context)
   })
 }
