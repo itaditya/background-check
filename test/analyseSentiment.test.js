5a98fe78cd9ab46330f41871b4e155b3e2a4c7d7
Aditya Agarwal <adityaa803@gmail.com>
2018-05-27 02:29:06 +0530

    test when request fails

--- test/analyseSentiment.test.js
+++ test/analyseSentiment.test.js
@@ -12,28 +12,60 @@ const TOXIC_MIN_VALUE = 0.6
 const ERROR_FLAG = -1
 
 describe('that analyseSentiment is working', () => {
-  test('that simple text works', async () => {
+  test('that simple text works', async() => {
     const text = "@itaditya I don't like the way you do things, your library is a joke"
     const toxicScore = await sentimentAnalyserInstance(text)
     expect(toxicScore).toBeGreaterThanOrEqual(toxicScore)
   })
-  test(`that undefined argument returns ${ERROR_FLAG}`, async () => {
+  test(`that undefined argument returns ${ERROR_FLAG}`, async() => {
     const toxicScore = await sentimentAnalyserInstance()
     expect(toxicScore).toBe(ERROR_FLAG)
   })
-  test(`that empty text returns ${ERROR_FLAG}`, async () => {
+  test(`that empty text returns ${ERROR_FLAG}`, async() => {
     const text = ''
     const toxicScore = await sentimentAnalyserInstance(text)
     expect(toxicScore).toBe(ERROR_FLAG)
   })
-  test(`that text of size more than 3000B returns ${ERROR_FLAG}`, async () => {
+  test(`that text of size more than 3000B returns ${ERROR_FLAG}`, async() => {
     let text = 'are you mad'.repeat(1000)
     const toxicScore = await sentimentAnalyserInstance(text)
     expect(toxicScore).toBe(ERROR_FLAG)
   })
-  test('that non toxic text is not considered toxic', async () => {
+  test('that non toxic text is not considered toxic', async() => {
     const text = "@itaditya I don't know if you are right on this"
     const toxicScore = await sentimentAnalyserInstance(text)
     expect(toxicScore).toBeLessThanOrEqual(toxicScore)
+  })
+  describe('that request errors are handled properly', () => {
+    test('that promise rejection is handled properly', async() => {
+      const text = "@itaditya I don't know if you are right on this"
+      const faultyRequest = {
+        post: function () { return this },
+        set: function () { return this },
+        send: () => {
+          return Promise.reject('error')
+        }
+      }
+      const fakeSentimentAnalyserInstance = analyseSentiment(PERSPECTIVE_API_KEY, {
+        dependencies: { request: faultyRequest }
+      })
+      const toxicScore = await fakeSentimentAnalyserInstance(text)
+      expect(toxicScore).toBe(ERROR_FLAG)
+    })
+    test('that throw is handled properly', async() => {
+      const text = "@itaditya I don't know if you are right on this"
+      const faultyRequest = {
+        post: function () { return this },
+        set: function () { return this },
+        send: () => {
+          throw 'error'
+        }
+      }
+      const fakeSentimentAnalyserInstance = analyseSentiment(PERSPECTIVE_API_KEY, {
+        dependencies: { request: faultyRequest }
+      })
+      const toxicScore = await fakeSentimentAnalyserInstance(text)
+      expect(toxicScore).toBe(ERROR_FLAG)
+    })
   })
 })
