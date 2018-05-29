f239c808ba6efcd429cedb0d205ae0747922ad9a
Aditya Agarwal <adityaa803@gmail.com>
2018-05-29 11:11:49 +0530

    implemented dependency injection to fix #7

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const backgroundFinder = require('./lib/backgroundFinder')
+const request = require('superagent')
+
+const extractRepoDetails = require('./lib/utils/extractRepoDetailsFromUrl')
+const analyseSentiment = require('./lib/utils/analyseSentiment')
+const getUserCommentedIssues = require('./lib/github-api/getUserCommentedIssues')
+const getCommentsOnIssue = require('./lib/github-api/getCommentsOnIssue')
+const backgroundFinder = require('./lib/backgroundFinder')({
+  dependencies: {
+    extractRepoDetails,
+    getUserCommentedIssues,
+    getCommentsOnIssue,
+    analyseSentiment,
+    request
+  }
+})
 
 module.exports = (robot) => {
   robot.log('bot started')
