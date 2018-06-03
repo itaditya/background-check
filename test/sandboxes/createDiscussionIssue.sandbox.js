26ad8b9fd5569ca656e7ab6ce12aad7b8027b59f
Aditya Agarwal <adityaa803@gmail.com>
2018-06-03 10:36:38 +0530

    added links and proper message formatting which fixes #28 (#34)

--- test/sandboxes/createDiscussionIssue.sandbox.js
+++ test/sandboxes/createDiscussionIssue.sandbox.js
@@ -1,9 +1,19 @@
 const createDiscussionIssue = require('../../lib/github-api/createDiscussionIssue')
 
 module.exports = async context => {
+  const toxicComments = [{
+    link: 'https://github.com/itaditya/gh-app-test-repo/issues/60',
+    text: "what is the meaning of this, are you mad? Seriously how could you create such a sinister thing. I curse that your bot doesn't work",
+    toxicScore: 0.7
+  }, {
+    link: 'https://github.com/itaditya/gh-app-test-repo/issues/60',
+    text: "what is the meaning of this, are you mad? Seriously how could you create such a sinister thing. I curse that your bot doesn't work",
+    toxicScore: 0.6
+  }]
   const result = await createDiscussionIssue(context, {
     owner: 'itaditya',
-    username: 'itaditya'
+    username: 'itaditya',
+    toxicComments
   })
   console.log(result)
 }
