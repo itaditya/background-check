2dbbcabd6e1ad9f5424b90c822bf6fd9e1764d30
Aditya Agarwal <adityaa803@gmail.com>
2018-06-26 19:41:18 +0530

    pass github client which fixes #52 (#53)

--- test/sandboxes/getPublicComments.sandbox.js
+++ test/sandboxes/getPublicComments.sandbox.js
@@ -1,7 +1,7 @@
 const getPublicComments = require('../../lib/getPublicComments')
 
 module.exports = async context => {
-  const comments = await getPublicComments(context, {
+  const comments = await getPublicComments(context.github, {
     username: 'itaditya'
   })
   console.log('First Comment: \n', comments[0])
