d09044ed146a59a218e9ad76d63f5347fc5c309d
Aditya Agarwal <adityaa803@gmail.com>
2018-07-05 21:36:10 +0530

    createDiscussionRepo github-api method created which fixes #56 (#57)
    * createDiscussionRepo github-api method created which fixes #56
    
    * made changes based on feedback

---
+++ test/createDiscussionRepo.test.js
@@ -0,0 +1,22 @@
+const createDiscussionRepo = require('../lib/github-api/createDiscussionRepo')
+
+test('createDiscussionRepo is working', async () => {
+  const github = {
+    repos: { createForOrg: jest.fn() }
+  }
+
+  // Call your function, which internally calls your above mock function
+  await createDiscussionRepo(github, { appInstallerName: 'test-org' })
+
+  // Test that your mock function has been called
+  expect(github.repos.createForOrg).toHaveBeenCalledWith({
+    org: 'probot-background-check',
+    name: 'test-org-discussions',
+    description: 'Repo to have discussions about toxic users',
+    // private: true,
+    auto_init: true
+  })
+
+  // Test that your mock function has been called with the right arguments
+  expect(github.repos.createForOrg.mock.calls).toMatchSnapshot()
+})
