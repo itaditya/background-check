d95b3717e13ebcda557ce05823fc424bcf26ff12
Aditya Agarwal <adityaa803@gmail.com>
2018-07-08 00:23:19 +0530

    updated org names which fixes #63

--- test/getUserDiscussionIssue.test.js
+++ test/getUserDiscussionIssue.test.js
@@ -1,10 +1,12 @@
 const getUserDiscussionIssue = require('../lib/github-api/getUserDiscussionIssue')
 
+process.env.APP_ORG_NAME = 'probot-background-check'
+
 test('getUserDiscussionIssue is working', async () => {
   const github = {
     search: {
       issues ({ q }) {
-        expect(q).toBe('repo:itaditya/maintainers-discussion is:issue is:open in:title itaditya-discussion')
+        expect(q).toBe('repo:probot-background-check/itaditya-discussions is:issue is:open in:title itaditya-discussion')
       }
     }
   }
