1877232f76693d1836cbbc8197dd484c24638b5f
Aditya Agarwal <adityaa803@gmail.com>
2018-07-07 21:16:48 +0530

    add collaborator on install which fixes #64 (#65)

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const getUserCommentedIssues = require('./lib/github-api/getUserCommentedIssues'
 const getCommentsOnIssue = require('./lib/github-api/getCommentsOnIssue')
 const createDiscussionIssue = require('./lib/github-api/createDiscussionIssue')
 const createDiscussionRepo = require('./lib/github-api/createDiscussionRepo')
+const addCollaboratorToRepo = require('./lib/github-api/addCollaboratorToRepo')
 
 const { PERSPECTIVE_API_KEY, GITHUB_ACCESS_TOKEN } = process.env
 const sentimentAnalyser = analyseSentiment(PERSPECTIVE_API_KEY, {
@@ -33,6 +34,7 @@ const backgroundFinder = require('./lib/backgroundFinder')({
 const discussionBoardSetup = require('./lib/discussionBoardSetup')({
   dependencies: {
     createDiscussionRepo,
+    addCollaboratorToRepo,
     githubClient
   }
 })
@@ -41,6 +43,7 @@ module.exports = (robot) => {
   robot.log('bot started')
 
   robot.on('installation.created', context => {
+    console.log('hello')
     discussionBoardSetup(context)
   })
 
